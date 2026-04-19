d55e967776931b7007abbb253099561edda0f14d
NANDITHA BANDI <nandithabandi@NANDITHAs-Air.lan>
2026-04-18 22:04:11 -0500

    resolve conflict

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "My DevOps AKS Project is running 🚀"
+    return "AKS DevOps Project Running 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
